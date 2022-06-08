88c74254fa2a2d5045f7ed491efdb1ba3b733b89
jonatanfranco <jonatan.adm@outlook.com>
2022-06-08 20:19:11 -0300

    at

---
+++ condicoes/01.js
@@ -0,0 +1,6 @@
+var vel = 60.8
+console.log('A velocidade do seu carro é ${vel} km' )
+if (vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO')
+}
+console.log('Dirija sempre com segurança')
