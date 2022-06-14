7fe4a109b030007d66c840e2b59af0cd88b56e0f
jonatanfranco <jonatan.adm@outlook.com>
2022-06-14 20:52:49 -0300

    creat 10

---
+++ condicoes/10.js
@@ -0,0 +1,16 @@
+function parimpar(n){
+    if (n%2==0){
+        return 'par'
+    }else {
+        return 'Impar'
+    }
+}
+console.log(parimpar(223))
+
+function soma(n1=0,n2=0){
+    return n1 + n2
+}
+console.log(soma(7+3))//7+3 posso colocar numero que quiser que soma
+
+//estudar sintase e recursidade
+
